ae396fb76ee09963b151bd861180e4dac96fc898
Mohammed <md.almeshal@gmail.com>
2021-02-28 00:46:46 +0300

    🌐 added prod route 7

--- src/flux/actions/authActions.js
+++ src/flux/actions/authActions.js
@@ -17,7 +17,7 @@ export const loadUser = () => (dispatch, getState) => {
   dispatch({ type: USER_LOADING });
 
   axios
-    .get(`/api/auth/user`, tokenConfig(getState))
+    .get(`https://jooba.herokuapp.com/api/auth/user`, tokenConfig(getState))
     .then((res) => {
       dispatch({ type: USER_LOADED, payload: res.data });
     })
