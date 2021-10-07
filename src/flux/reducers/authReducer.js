831599b2b3a9216e37c054716e6dbe67df55de6d
Mohammed <md.almeshal@gmail.com>
2021-10-07 11:45:56 +0300

    fix: redirect

--- src/flux/reducers/authReducer.js
+++ src/flux/reducers/authReducer.js
@@ -1,60 +1,66 @@
 import {
-    USER_LOADING,
-    AUTH_ERROR,
-    USER_LOADED,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT_SUCCESS,
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-  } from "../actions/types";
-  
-  const initialState = {
-    token: localStorage.getItem("token"),
-    isAuthenticated: null,
-    isLoading: null,
-    user: {
-      username:''
-    },
-  };
-  
-  export default function (state = initialState, action) {
-    switch (action.type) {
-      case USER_LOADING:
-        return {
-          ...state,
-          isLoading: true,
-        };
-      case USER_LOADED:
-        return {
-          ...state,
-          isAuthenticated: true,
-          isLoading: false,
-          user: action.payload,
-        };
-      case LOGIN_SUCCESS:
-      case REGISTER_SUCCESS:
-        localStorage.setItem("token", action.payload.token);
-        return {
-          ...state,
-          ...action.payload,
-          isAuthenticated: true,
-          isLoading: false,
-        };
-      case AUTH_ERROR:
-      case LOGIN_FAIL:
-      case REGISTER_FAIL:
-      case LOGOUT_SUCCESS:
-        localStorage.removeItem("token");
-        return {
-          ...state,
-          token: null,
-          user: '',
-          isAuthenticated: false,
-          isLoading: false,
-        };
-      default:
-        return state;
-    }
+  USER_LOADING,
+  AUTH_ERROR,
+  USER_LOADED,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+} from "../actions/types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: null,
+  user: {
+    username: "",
+  },
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case REGISTER_FAIL:
+    case LOGOUT_SUCCESS:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        user: "",
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    default:
+      return state;
   }
-  
+}
